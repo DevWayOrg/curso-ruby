6a816845d050eb58ddfbbcda678c254d5b5dfcf2
vinibispo <vini.bispo015@gmail.com>
2023-06-05 14:12:27 -0300

    feat(types): add each type with puts type on it

--- app.rb
+++ app.rb
@@ -1,3 +1,69 @@
-p 'A'
-puts 'C'
-print 'B'
+# String
+phrase = "Hello World"
+puts "String"
+p phrase
+
+puts "=" * 80
+
+# Integer
+
+num = 1_000
+puts "Integer"
+p num
+
+puts "=" * 80
+
+# Float
+
+num2 = -210.20
+puts "Float"
+p num2
+
+puts "=" * 80
+# Booleans
+# TrueClass
+
+is_raining = true
+puts "TrueClass"
+p is_raining
+
+puts "=" * 80
+
+# FalseClass
+is_sunny = false
+puts "FalseClass"
+p is_sunny
+
+puts "=" * 80
+# Null
+# NilClass
+
+user = nil
+puts "NilClass"
+p user
+
+puts "=" * 80
+# Array
+
+arr = [1,2,3] # Array.new
+puts "Array"
+p arr
+
+puts "=" * 80
+
+# Hash
+
+hash = {hello: "world"} # Hash.new
+puts "Hash"
+p hash
+
+hash = {"hello" => "world"}
+p hash
+
+puts "=" * 80
+
+# Symbol
+
+my_symbol = :ok
+puts "Symbol"
+p my_symbol
