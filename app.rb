77008f437791189d93c212e57da6870ca6aebb12
vinibispo <vini.bispo015@gmail.com>
2023-06-15 09:59:24 -0300

    feat(game): adding loops to our game

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 ROCK = 0
 PAPER = 1
 SCISSORS = 2
+QUIT = 'Q'
 
 CONVERSION = {
   'R' => ROCK,
@@ -15,27 +16,33 @@ NUMBER_CONVERSION = {
 }.freeze
 
 NUM_CHOICES = 3
-computer_choice = rand(3) # 0, 1, or 2
+loop do
+  computer_choice = rand(3) # 0, 1, or 2
 
-puts "Let's play Rock, Paper, Scissors!"
-puts 'Enter your choice: (R)ock, (P)aper, or (S)cissors'
-puts 'If you enter a wrong option, we will play Rock for you!'
-user_choice = gets.chomp
-choice = CONVERSION[user_choice] || ROCK
+  puts "Let's play Rock, Paper, Scissors!"
+  puts 'Enter your choice: (R)ock, (P)aper, (S)cissors, or (Q)uit'
+  puts 'If you enter a wrong option, we will play Rock for you!'
+  user_choice = gets.chomp
+  break if user_choice == QUIT
 
-puts "You chose #{NUMBER_CONVERSION[choice]} and the computer chose #{NUMBER_CONVERSION[computer_choice]}"
+  choice = CONVERSION[user_choice] || ROCK
 
-case computer_choice
-when ROCK
-  puts 'You win!' if choice == PAPER
-  puts 'You lose!' if choice == SCISSORS
-  puts "It's a tie!" if choice == ROCK
-when PAPER
-  puts 'You win!' if choice == SCISSORS
-  puts 'You lose!' if choice == ROCK
-  puts "It's a tie!" if choice == PAPER
-when SCISSORS
-  puts 'You win!' if choice == ROCK
-  puts 'You lose!' if choice == PAPER
-  puts "It's a tie!" if choice == SCISSORS
+  puts "You chose #{NUMBER_CONVERSION[choice]} and the computer chose #{NUMBER_CONVERSION[computer_choice]}"
+
+  case computer_choice
+  when ROCK
+    puts 'You win!' if choice == PAPER
+    puts 'You lose!' if choice == SCISSORS
+    puts "It's a tie!" if choice == ROCK
+  when PAPER
+    puts 'You win!' if choice == SCISSORS
+    puts 'You lose!' if choice == ROCK
+    puts "It's a tie!" if choice == PAPER
+  when SCISSORS
+    puts 'You win!' if choice == ROCK
+    puts 'You lose!' if choice == PAPER
+    puts "It's a tie!" if choice == SCISSORS
+  end
+  puts '==================='
+  puts "\n" * 10
 end
